67094a13d6d99fea7feb5940b1e7020609fd7304
evertonkoper <everton.rivas@koper.com.br>
2025-11-28 16:57:02 -0300

    a lot corrections

--- smc/users.py
+++ smc/users.py
@@ -6,10 +6,11 @@ from http import HTTPStatus
 from datetime import datetime
 from models.helpers import _get_params, db
 from flask_restx import Resource ,Namespace, fields
-from f2bconfig import CustomerAction, MailTemplates, UserType
+from f2bconfig import CustomerAction, DashboardImage, DashboardImageColor, LegalEntityContactType, MailTemplates, UserType
 from common import _send_email, _get_dashboard_config
 from models.public import _save_customer_log, SysConfig
 from models.public import SysUsers, SysCustomer, SysCustomerUser, SysPlan, SysCustomerPlan
+from models.tenant import CmmLegalEntities, CmmLegalEntityContact
 from sqlalchemy import Delete, Select, desc, exc, and_, asc, func, or_
 
 ns_user = Namespace("users",description="Operações para manipular dados de usuários do sistema")
@@ -81,6 +82,7 @@ class UsersList(Resource):
                 SysUsers.id,
                 SysUsers.name,
                 SysUsers.username,
+                SysUsers.email,
                 SysUsers.type,
                 SysUsers.date_created,
                 SysUsers.date_updated,
@@ -109,6 +111,7 @@ class UsersList(Resource):
                         "id": m.id,
                         "name": m.name,
                         "username": m.username,
+                        "email": m.email,
                         "type": m.type,
                         "active": m.active,
                         "date_created": m.date_created.strftime("%Y-%m-%d %H:%M:%S"),
@@ -120,6 +123,7 @@ class UsersList(Resource):
                     "id": m.id,
                     "name": m.name,
                     "username": m.username,
+                    "email": m.email,
                     "type": m.type,
                     "active": m.active,
                     "date_created": m.date_created.strftime("%Y-%m-%d %H:%M:%S"),
@@ -144,8 +148,13 @@ class UsersList(Resource):
 
                 # primeiramente busca o numero de licensas do plano assinado pelo cliente
                 plan = db.session.execute(
-                    Select(SysPlan.adm_licenses,SysPlan.user_licenses,SysPlan.store_licenses,SysPlan.istore_licenses,SysPlan.repr_licenses).where(
-                        SysPlan.id==(Select(SysCustomerPlan.id_plan).where(SysCustomerPlan.id_customer==req['id_customer']))
+                    Select(SysPlan.adm_licenses,
+                           SysPlan.user_licenses,
+                           SysPlan.store_licenses,
+                           SysPlan.istore_licenses,
+                           SysPlan.repr_licenses
+                        ).where(
+                        SysPlan.id==(Select(SysCustomerPlan.id_plan).where(SysCustomerPlan.id_customer==request.headers.get('x-customer')))
                     )
                 ).first()
 
@@ -160,24 +169,24 @@ class UsersList(Resource):
                             "error_details": "Número máximo de licenças Adm. atingido!",
                             "error_sql": ""
                         }
-                    elif usr["type"]==UserType.REPRESENTATIVE.value and total.total_lic == int(plan.repr_licenses):
-                        return {
-                            "error_code": -1,
-                            "error_details": "Número máximo de licenças REP. atingido!",
-                            "error_sql": ""
-                        }
-                    elif usr["type"]==UserType.ISTORE.value and total.total_lic == int(plan.istore_licenses):
-                        return {
-                            "error_code": -1,
-                            "error_details": "Número máximo de licenças I.A atingido!",
-                            "error_sql": ""
-                        }
-                    elif usr["type"]==UserType.STORE.value and total.total_lic == int(plan.store_licenses):
-                        return {
-                            "error_code": -1,
-                            "error_details": "Número máximo de licenças Lojista atingido!",
-                            "error_sql": ""
-                        }
+                    # elif usr["type"]==UserType.REPRESENTATIVE.value and total.total_lic == int(plan.repr_licenses):
+                    #     return {
+                    #         "error_code": -1,
+                    #         "error_details": "Número máximo de licenças REP. atingido!",
+                    #         "error_sql": ""
+                    #     }
+                    # elif usr["type"]==UserType.ISTORE.value and total.total_lic == int(plan.istore_licenses):
+                    #     return {
+                    #         "error_code": -1,
+                    #         "error_details": "Número máximo de licenças I.A atingido!",
+                    #         "error_sql": ""
+                    #     }
+                    # elif usr["type"]==UserType.STORE.value and total.total_lic == int(plan.store_licenses):
+                    #     return {
+                    #         "error_code": -1,
+                    #         "error_details": "Número máximo de licenças Lojista/Empresa atingido!",
+                    #         "error_sql": ""
+                    #     }
                     elif usr["type"]==UserType.COMPANY_USER.value and total.total_lic == int(plan.user_licenses):
                         return {
                             "error_code": -1,
@@ -336,6 +345,7 @@ class UserAuth(Resource):
     def post(self):
         req = request.get_json()
         
+        # busca os dados do usuario e da conta de customer deles atraves do usuario, email e CNPJ
         query = Select(SysUsers,SysCustomerUser)\
                      .join(SysCustomerUser,SysCustomerUser.id_user==SysUsers.id)\
                      .join(SysCustomer,SysCustomer.id==SysCustomerUser.id_customer)\
@@ -343,17 +353,40 @@ class UserAuth(Resource):
                      .where(or_(
                          SysUsers.username==req["username"],
                          SysUsers.email==req["username"],
-                         SysCustomer.taxvat==req["username"]
+                         SysCustomer.taxvat==str(request.form.get("username")).replace(".","").replace("-","").replace("/","")
                      ))
         usr = db.session.execute(query).first()
-        # usr = SysUsers.query.filter(and_(SysUsers.username==request.form.get("username"),SysUsers.active==True)).first()
+        
+        # se o usuario existir
         if usr is not None:
+            # dependendo do tipo ira buscas as entidades de negocio
+            entity = 0
+            if usr[0].type==UserType.STORE.value or usr[0].type==UserType.ISTORE.value or usr[0].type==UserType.REPRESENTATIVE:
+                entity = db.session.execute(Select(CmmLegalEntities.id).where(CmmLegalEntities.id_user==usr[0].id)).first()
 
-            #cfg = db.session.execute(Select(SysConfig).where(SysConfig.id_customer==usr[1].id_customer)).first()
-            url =str(environ.get("F2B_SMC_URL"))+"/config/"+str(usr[1].id_customer)
-            cfg_req = requests.get(url)
-            if cfg_req.status_code==HTTPStatus.OK.value:
-                cfg = cfg_req.json()
+            # busca as configuracoes do usuario
+            cfg = db.session.execute(Select(SysConfig).where(SysConfig.id_customer==str(usr[1].id_customer))).first()
+            if cfg[0].dashboard_config=='M':
+                dash_image = DashboardImage.MEN.value
+                dash_color = DashboardImageColor.MEN.value
+            if cfg[0].dashboard_config=='W':
+                dash_image = DashboardImage.WOMEN.value
+                dash_color = DashboardImageColor.WOMEN.value
+            if cfg[0].dashboard_config=='H':
+                dash_image = DashboardImage.WHEAT.value
+                dash_color = DashboardImageColor.WHEAT.value
+            if cfg[0].dashboard_config=='D':
+                dash_image = DashboardImage.DRINK.value
+                dash_color = DashboardImageColor.DRINK.value
+            if cfg[0].dashboard_config=='S':
+                dash_image = DashboardImage.SHOES.value
+                dash_color = DashboardImageColor.SHOES.value
+            if cfg[0].dashboard_config=='P':
+                dash_image = DashboardImage.PISTON.value
+                dash_color = DashboardImageColor.PISTON.value
+            if cfg[0].dashboard_config=='F':
+                dash_image = DashboardImage.PHARMA.value
+                dash_color = DashboardImageColor.PHARMA.value
 
             #verifica a senha criptografada anteriormente
             pwd = str(req["password"]).encode()
@@ -362,10 +395,41 @@ class UserAuth(Resource):
 					"token_access": usr[0].get_token(str(usr[1].id_customer)),
 					"token_type": "Bearer",
 					"token_expire": usr[0].token_expire.strftime("%Y-%m-%d %H:%M:%S"),
-					"level_access": usr[0].type,
+					"user_type": usr[0].type,
                     "id_user": usr[0].id,
                     "id_profile": str(usr[1].id_customer),
-                    "config": cfg
+                    "id_entity": entity,
+                    "config": {
+                        "id_customer": str(cfg[0].id_customer),
+                        "pagination_size": cfg[0].pagination_size,
+                        "email_brevo_api_key": cfg[0].email_brevo_api_key,
+                        "email_from_name": cfg[0].email_from_name,
+                        "email_from_value": cfg[0].email_from_value,
+                        "flimv_model": cfg[0].flimv_model,
+                        "dashboard_config": cfg[0].dashboard_config,
+                        "dashboard_image": dash_image,
+                        "dashboard_color": dash_color,
+                        "ai_model": cfg[0].ai_model,
+                        "ai_api_key": cfg[0].ai_api_key,
+                        "company_custom": cfg[0].company_custom,
+                        "company_name": cfg[0].company_name,
+                        "company_logo": cfg[0].company_logo,
+                        "url_instagram": cfg[0].url_instagram,
+                        "url_facebook": cfg[0].url_facebook,
+                        "url_linkedin": cfg[0].url_linkedin,
+                        "max_upload_files": cfg[0].max_upload_files,
+                        "max_upload_images": cfg[0].max_upload_images,
+                        "use_url_images": cfg[0].use_url_images,
+                        "track_orders": cfg[0].track_orders,
+                        "erp_integration": cfg[0].erp_integration,
+                        "erp_url": cfg[0].erp_url,
+                        "erp_token": cfg[0].erp_token,
+                        "erp_grant_type": cfg[0].erp_grant_type,
+                        "erp_client_id": cfg[0].erp_client_id,
+                        "erp_client_secret": cfg[0].erp_client_secret,
+                        "erp_username": cfg[0].erp_username,
+                        "erp_password": cfg[0].erp_password
+                    }
                 }
                 usr[0].is_authenticate = True
                 db.session.commit()
@@ -373,28 +437,6 @@ class UserAuth(Resource):
                 return obj_retorno
             else:
                 return 0 #senha invalida
-        else:
-            #tenta encontrar a entidade pelo usuario ou CNPJ/CPF
-            entity = db.session.execute(Select(SysCustomer.id).distinct()\
-                .where(SysCustomer.taxvat==str(request.form.get("username")).replace(".","").replace("-","").replace("/",""))).first()
-            if entity is not None:
-                usr = SysUsers.query.filter(and_(SysUsers.id==(Select(SysCustomerUser.id_user).where(SysCustomerUser.id_customer==entity.id)),SysUsers.active.is_(True))).first()
-                if usr is not None:
-                    #verifica a senha criptografada anteriormente
-                    pwd = str(request.form.get("password")).encode()
-                    if usr.check_pwd(pwd):
-                        obj_retorno = {
-                            "token_access": usr.get_token(str(entity.id)),
-                            "token_type": "Bearer",
-                            "token_expire": usr.token_expire.strftime("%Y-%m-%d %H:%M:%S"),
-                            "level_access": usr.type,
-                            "id_user": usr.id,
-                            "id_profile": str(entity.id)
-                        }
-                        usr.is_authenticate = True
-                        db.session.commit()
-                        _save_customer_log(usr.id,entity.id,CustomerAction.SYSTEM_ACCESS,'Efetuou login')
-                        return obj_retorno
         return -1 #usuario invalido
     
     @ns_user.response(HTTPStatus.OK,"Realiza a validacao do token do usuario")
@@ -420,7 +462,6 @@ class UserAuth(Resource):
         except Exception as e:
             print(e)
             return False
-
 ns_user.add_resource(UserAuth,"/auth")
 
 @ns_user.param("id","Id do registro")
@@ -444,7 +485,7 @@ ns_user.add_resource(UserAuthLogout,"/logout/<int:id>")
 
 class UserUpdate(Resource):
     def __get_username(self,id,rule):
-        reg = db.session.execute(Select(SysCustomer.name).where(SysCustomer.id==id)).first()
+        reg = db.session.execute(Select(CmmLegalEntities.name).where(CmmLegalEntities.id==id)).first()
         if reg is not None:
             name = reg.name
             name = str(name).replace(".","")
@@ -477,39 +518,83 @@ class UserUpdate(Resource):
             req = request.get_json()
             for id_entity in req["ids"]:
 
-                exist_entity = db.session.execute(
-                    Select(func.count().label("total")).select_from(SysCustomerUser)\
-                        .where(SysCustomerUser.id_customer==id_entity)
+                #busca o total de licensas do plano
+                plan = db.session.execute(
+                    Select(SysPlan.adm_licenses,
+                           SysPlan.user_licenses,
+                           SysPlan.store_licenses,
+                           SysPlan.istore_licenses,
+                           SysPlan.repr_licenses
+                        ).where(
+                        SysPlan.id==(Select(SysCustomerPlan.id_plan).where(SysCustomerPlan.id_customer==request.headers.get('x-customer')  ))
+                    )
                 ).first()
 
-                if exist_entity is None or exist_entity.total == 0:
+                # busca o total de licencas existentes por tipo de usuario
+                total = db.session.execute(
+                    Select(func.count(SysUsers.id).label("total_lic")).where(SysUsers.type==usr["type"])
+                ).first()
+                if total is not None:
+                    if usr["type"]==UserType.REPRESENTATIVE.value:
+                        if plan.repr_licenses!=-1 and total.total_lic == int(plan.repr_licenses):
+                            return {
+                                "error_code": -1,
+                                "error_details": "Número máximo de licenças REP. atingido!",
+                                "error_sql": ""
+                            }
+                    elif usr["type"]==UserType.ISTORE.value:
+                        if plan.istore_licenses!=-1 and total.total_lic == int(plan.istore_licenses):
+                            return {
+                                "error_code": -1,
+                                "error_details": "Número máximo de licenças Lojista/Empresa (I.A) atingido!",
+                                "error_sql": ""
+                            }
+                    elif usr["type"]==UserType.STORE.value and total.total_lic:
+                        if plan.store_license!=-1 and int(plan.store_licenses):
+                            return {
+                                "error_code": -1,
+                                "error_details": "Número máximo de licenças Lojista/Empresa atingido!",
+                                "error_sql": ""
+                            }
 
-                    exist_uname = db.session.execute(
-                        Select(SysUsers.username,SysUsers.id)\
-                        .where(SysUsers.username==self.__get_username(id_entity,req["rule"]))
-                    ).first()
 
-                    if exist_uname is None:
-                        usr = SysUsers()
-                        setattr(usr,"username",(self.__get_username(id_entity,req["rule"])))
-                        usr.password = req["password"]
-                        setattr(usr,"type",req["type"])
-                        db.session.add(usr)
-                        db.session.commit()
+                # verifica se o usuario jah existe no sistema pelo username/email
+                exist_user = db.session.execute(
+                    Select(SysUsers.username,SysUsers.id)\
+                    .where(SysUsers.email.in_(
+                        Select(CmmLegalEntityContact.value).where(CmmLegalEntityContact.contact_type==LegalEntityContactType.EMAIL.value)
+                    ))
+                ).first()
 
-                        usrE = SysCustomerUser()
-                        usrE.id_user   = usr.id
-                        usrE.id_customer = id_entity
-                        db.session.add(usrE)
-                        db.session.commit()
+    
+                # se ainda nao existe usuario cria
+                if exist_user is None:
+                    usr = SysUsers()
+                    setattr(usr,"username",(self.__get_username(id_entity,req["rule"])))
+                    usr.hash_pwd(req["password"])
+                    setattr(usr,"type",req["type"])
+                    db.session.add(usr)
+                    db.session.commit()
+
+                    # adiciona o usuario para efetuar login no sistema
+                    usrC = SysCustomerUser()
+                    usrC.id_user   = usr.id
+                    usrC.id_customer = request.headers.get("x-customer")
+                    db.session.add(usrC)
+                    db.session.commit()
+
+                    # atualiza a tabela de entidades para receber o id do usuario
+                    usrE:CmmLegalEntities = CmmLegalEntities.query.get(id_entity)
+                    usrE.id_user = usr.id
+                    db.session.commit()
+
                 else:
                     # atualiza apenas o username e o password
                     usrE = db.session.execute(Select(SysCustomerUser.id_user).where(SysCustomerUser.id_customer==id_entity)).first()
                     usr:SysUsers|None = SysUsers.query.get(0 if usrE is None else usrE.id_user)
                     if usr is not None:
-                        usr.password = req["password"]
+                        usr.hash_pwd(req["password"])
                         setattr(usr,"username",self.__get_username(id_entity,req["rule"]))
-                        setattr(usr,"type",req["type"])
                         db.session.commit()
             return True
         except exc.SQLAlchemyError as e:
@@ -641,7 +726,6 @@ class UserPassword(Resource):
                 "error_details": e._message(),
                 "error_sql": e._sql_message()
             }
-
 ns_user.add_resource(UserPassword,"/password/")
 
 class UserCount(Resource):
